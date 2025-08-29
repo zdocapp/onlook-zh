6c35a9fdcc32276bdde7e51c1c4332b17ce210f3
neo <neo.dowithless@gmail.com>
2025-08-29 15:00:58 +0800

    perf: update sitemap config

--- docs/next-sitemap.config.js
+++ docs/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://docs.onlook.dev',
-  generateRobotsTxt: false, // handled by route handler
-  generateIndexSitemap: true,
+    siteUrl: 'https://onlook-zh.zdoc.app',
+    generateRobotsTxt: false, // handled by route handler
+    generateIndexSitemap: true,
 };
